6212615ae004a91bd5237383281387d212076e70
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2024-04-19 11:04:30 +0100

    - docs

--- src/TrackerPi/Options.cs
+++ src/TrackerPi/Options.cs
@@ -4,9 +4,9 @@ using CommandLine;
 
 internal sealed class Options
 {
-  [Value(index: 0, Required = true, HelpText = "GPS Port eg ttyACM0")]
+  [Value(index: 0, Required = true, HelpText = "GPS Port eg /dev/ttyACM0")]
   public string GpsPort { get; set; }
   
-  [Value(index: 1, Required = true, HelpText = "OBD Port eg ttyOBD0")]
+  [Value(index: 1, Required = true, HelpText = "OBD Port eg /dev/ttyUSB0")]
   public string ObdPort { get; set; }
 }
